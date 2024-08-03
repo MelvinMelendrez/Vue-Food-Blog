e249266858ecad11a026a7bb889d6e9addb457e2
MelvinMelendrez <melvin_sss@hotmail.com>
2024-08-03 01:19:55 -0700

    search by name component

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,5 +1,12 @@
-function meals() {
-  return;
+export function setSearchedMeals(state, meals) {
+  state.searchedMeals = meals || [];
 }
-
-export default meals;
+export function setMealsByLetter(state, meals) {
+  state.mealsByLetter = meals || [];
+}
+export function setMealsByIngredients(state, meals) {
+  state.mealsByIngredient = meals || [];
+}
+export function setIngredient(state, ingredient) {
+  state.ingredient = ingredient;
+}
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,15 +1,45 @@
 <template>
-  <div>
-    <div class="flex p-8 justify-center">
-      <input
-        type="text"
-        class="rounded border-2 border-gray-200 w-full"
-        placeholder="Search for Meals..."
-      />
-    </div>
+  <div class="p-8 pb-0">
+    <h1 class="text-4xl font-bold mb-4 text-orange-500">
+      Search Meals by Name
+    </h1>
   </div>
+  <div class="px-8 pb-3">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded text-black border-2 bg-white border-gray-200 focus:ring-orange-500 focus:border-orange-500 w-full"
+      placeholder="Search for Meals"
+      @keyup.enter="searchMeals"
+    />
+  </div>
+  <pre>{{ meals }}</pre>
+  <!-- <Meals :meals="meals" /> -->
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from "vue";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import store from "../store";
 
-<style lang="scss" scoped></style>
+const route = useRoute();
+const keyword = ref("");
+const meals = computed(() => store.state.searchedMeals);
+
+function searchMeals() {
+  if (keyword.value) {
+    store.dispatch("searchMeals", keyword.value);
+    keyword.value = "";
+  } else {
+    store.commit("setSearchedMeals", []);
+  }
+}
+
+onMounted(() => {
+  keyword.value = route.params.name || "";
+  if (keyword.value) {
+    searchMeals();
+  }
+});
+</script>
